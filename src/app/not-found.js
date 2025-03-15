df226d74223039f7839da961ff64079022a14aa6
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2025-03-15 10:03:59 +0700

    route

---
+++ src/app/not-found.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button, Result } from "antd";
+import { useRouter } from "next/navigation";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  const handleRedirectToLogin = () => {
+    router.push("/dang-nhap");
+  };
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      className="bg-sky-50"
+    >
+      <Result
+        status="404"
+        title="404"
+        subTitle="Xin lỗi, trang bạn đang tìm kiếm không tồn tại."
+        extra={
+          <Button type="primary" onClick={handleRedirectToLogin}>
+            Đăng nhập
+          </Button>
+        }
+      />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,27 @@
 "use client";
 import "@ant-design/v5-patch-for-react-19";
-
+import { useEffect } from "react";
 import { Layout } from "antd";
-import Header from "./components/customer/Header";
-import HeroBanner from "./components/customer/landingPage/HeroBanner";
-import Features from "./components/customer/landingPage/Features";
-import ProductShowcase from "./components/customer/landingPage/ProductShowcase";
-import Testimonials from "./components/customer/landingPage/Testimonials";
-import Newsletter from "./components/customer/landingPage/Newsletter";
-import Footer from "./components/customer/Footer";
+import { useRouter } from "next/navigation";
 
 const { Content } = Layout;
 
 export default function CustomerLandingPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if the user is logged in
+    const token = localStorage.getItem("token"); // Assuming token is stored in localStorage
+
+    // If no token is found, redirect to login page
+    if (!token) {
+      router.push("/dang-nhap"); // Change this path if your login route is different
+    }
+  }, [router]);
+
   return (
     <Layout className="landing-page App">
-      <Header />
-      <Content>
-        <HeroBanner />
-        <Features />
-        <ProductShowcase />
-        <Testimonials />
-        <Newsletter />
-      </Content>
-      <Footer />
+      <Content>{/* Your landing page content will go here */}</Content>
     </Layout>
   );
 }
